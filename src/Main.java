0f44c223a42b6b90f9fcb61f761808e3a5b515f0
Ben Youngblut <ben.youngblut@automatiq.com>
2025-08-16 21:10:57 -0500

    New change branch

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.printf("Hello and brandon!");
-        for (int i = 1; i <= 5; i++) {
+        for (int i = 1; i <= 15; i++) {
             System.out.println("i = " + i);
         }
     }
